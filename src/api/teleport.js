97c3782d75296b38a2df3b2736f4952f1c55e621
cindybox <cindybox.33@gmail.com>
2019-06-06 12:44:19 -0500

    updated api with axois

--- src/api/teleport.js
+++ src/api/teleport.js
@@ -1,7 +1,45 @@
-fetch('https://api.teleport.org/api/')
-    .then(function(response) {
-        return response.json();
+import axios from "axios";
+
+async function getCityApi(cityname){
+  //first part of the Api extract basic data about the city, such as population, location
+  const searchUrl =
+            "https://api.teleport.org/api/cities/?search=" + "Houston"; //change this to match with search bar user input variable
+ const res1 = await axios
+        .get(searchUrl)
+        .then(response => {
+
+        const cityUrl = response.data["_embedded"]["city:search-results"][0][
+          "_links"
+        ]["city:item"]["href"];
+        return axios.get(cityUrl);
+      });
+
+ //second part of the Api extract urban scores
+const urUrl = res1.data["_links"]["city:urban_area"]["href"];
+
+const res2 = await
+axios.get(urUrl)
+.then(response => {
+      this.response = response.data;
+      const scoreUrl = this.response["_links"]["ua:scores"]["href"];
+      return axios.get(scoreUrl);
     })
-    .then(function(myJSON){
-        console.log(JSON.stringify(myJSON));
-    })
+
+//return an object with all city data.
+const urbanscores = res2.data.categories;
+const geohash = res1.data.location.geohash;
+const population = res1.data.population;
+const lat = res1.data.location.latlon.latitude;
+const lgn = res1.data.location.latlon.longitude;
+const name = res1.data.name;
+return {
+  name:name,
+  population:population,
+  urbanscores:urbanscores,
+  geohash:geohash,
+  lat:lat,
+  lgn:lgn
+  };
+}
+
+export default getCityApi;

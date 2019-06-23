08d24c37d53f824b2032b0c0851603988dab6267
abrown945 <abrown945@gmail.com>
2019-06-23 15:04:13 -0400

    Loads available climate data

--- src/components/Climate/Climate.js
+++ src/components/Climate/Climate.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const Climate = () => {
+const Climate = ({weatherType, avgHigh, avgLow, rainDays, sunnyChance, dayLength}) => {
+    if(!weatherType) {
+        return <div></div>
+    }
+
     return(
         <div>
-
+            <div className="card-body">
+                <div className="card-title">
+                    Climate
+                </div>
+                <div className="card-text">
+                    <p>Type of Climate: {weatherType}</p>
+                    <p>Average High: {avgHigh} C°</p>
+                    <p>Average Low: {avgLow} C°</p>
+                    <p>Average Number of Rainy Days Per Year: {rainDays}</p>
+                    <p>Annual Percent Chance of Sunshine: {sunnyChance}%</p>
+                    <p>Average Length of Day: {dayLength} Hours</p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Climate/ClimateContainer.js
+++ src/components/Climate/ClimateContainer.js
@@ -3,13 +3,95 @@ import teleport from '../../api/teleport';
 import Climate from './Climate';
 
 class ClimateContainer extends React.Component {
-    state = { loadSuccess: false }
+    state = { weatherType: '', avgHigh: '', avgLow: '', rainDays: 0, sunnyDays: 0, dayLength: 0, loadSuccess: false, loadedCityURL: '' }
+
+    climateDetails = async (city) => {
+        debugger;
+        let chosenCity = city;
+        let weatherType, avgHighTemp, avgLowTemp, rainDayCount, sunnyDayCount, dayHourAvg, success;
+        //navigation of teleport API to Safety section for chosen city
+        let cityDetails = await teleport.get(chosenCity);
+        cityDetails = cityDetails.data["_links"]["ua:details"]["href"];
+
+        let cityClimate = await teleport.get(cityDetails);
+        cityClimate = cityClimate.data.categories.find(category => category.id.toUpperCase() === "CLIMATE");
+
+        if(cityClimate) {
+            let checkType, checkHighTemp, checkLowTemp, checkRainDays, checkSunDays, checkDayHours;
+
+            checkType = cityClimate.data.find(x => x.id === "WEATHER-TYPE");            
+            if(checkType) {
+                weatherType = checkType.string_value;
+            }
+
+            checkHighTemp = cityClimate.data.find(x => x.id === "WEATHER-AVERAGE-HIGH");
+            if (checkHighTemp) {
+                avgHighTemp = checkHighTemp.string_value;
+            }
+
+            checkLowTemp = cityClimate.data.find(x => x.id === "WEATHER-AVERAGE-LOW");
+            if (checkLowTemp) {
+                avgLowTemp = checkLowTemp.string_value;
+            }
+
+            checkRainDays = cityClimate.data.find(x => x.id === "WEATHER-AV-NUMBER-RAINY-DAYS");
+            if (checkRainDays) {
+                rainDayCount = checkRainDays.float_value;
+            }
+
+            checkSunDays = cityClimate.data.find(x => x.id === "WEATHER-AV-NUMBER-CLEAR-DAYS");
+            if (checkSunDays) {
+                sunnyDayCount = checkSunDays.float_value;
+            }
+
+            checkDayHours = cityClimate.data.find(x => x.id === "");
+            if(checkDayHours) {
+                dayHourAvg = checkDayHours.float_value;
+            }
+          
+            success = true;
+        }
+
+        if (!cityClimate) {
+            weatherType = 'N/A';
+            avgHighTemp = 'N/A';
+            avgLowTemp = 'N/A';
+            rainDayCount = 0;
+            sunnyDayCount = 0;
+            dayHourAvg = 0;
+            success = false;
+        }
+
+        this.setState({
+            weatherType: weatherType, 
+            avgHigh: avgHighTemp, 
+            avgLow: avgLowTemp, 
+            rainDays: rainDayCount, 
+            sunnyDays: sunnyDayCount, 
+            dayLength: dayHourAvg, 
+            loadSuccess: success, 
+            loadedCityURL: chosenCity
+        })
+    }
 
     render() {
+        if (this.props.city && this.state.loadedCityURL !== this.props.city) {
+            this.climateDetails(this.props.city);
+        }
         let showClimate = null;
 
         if (this.state.loadSuccess) {
-            showClimate = (<div className="card"><Climate /></div>)
+            showClimate = (
+                <div className="card">
+                    <Climate 
+                        weatherType={this.state.weatherType}
+                        avgHigh={this.state.avgHigh}
+                        avgLow={this.state.avgLow}
+                        rainDays={this.state.rainDays}
+                        sunnyChance={this.state.sunnyChance}
+                        dayLength={this.state.dayLength} />
+                </div>
+            )
         }
 
         return <div>{showClimate}</div>

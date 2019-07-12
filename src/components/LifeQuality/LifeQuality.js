0d64abec9e1f05c6d1dbcc12c94b4e060b9c1bb2
abrown945 <abrown945@gmail.com>
2019-07-12 10:33:29 -0400

    reformating/fix duplication error

--- src/components/LifeQuality/LifeQuality.js
+++ src/components/LifeQuality/LifeQuality.js
@@ -1,50 +1,21 @@
-import './LifeQuality.css';
-import React from 'react'; 
-import LifeQualityScore from './LifeQualityScore';
+import "./LifeQuality.css";
+import React from "react";
+import LifeQualityScore from "./LifeQualityScore";
 
+const LifeQuality = ({cityScores}) => {
 
-class LifeQuality extends React.Component {
-    state = {scoreList: [], loadedCity: '', renderList: []}
+    const renderList = cityScores.map(score => {
+            return (
+                <LifeQualityScore
+                    key={score.name}
+                    name={score.name}
+                    score={score.score}
+                    color={score.color}
+                />
+            );
+        });
 
-    componentDidMount = async () => {
-        const {scores, city} = this.props;
-        this.setState({
-            scoreList: scores,
-            loadedCity: city
-        })
-    }
-
-    updateScores = (scores, city) => {
-        const categoryScores = scores;
-        const loadedScores = categoryScores.map((score) => {
-            return <LifeQualityScore
-                key={score.name}
-                name={score.name}
-                score={score.score}
-                color={score.color}
-            />;
-        })
-
-        this.setState({
-            scoreList: categoryScores, 
-            loadedCity: city,
-            renderList: loadedScores
-        })
-    }
-
-    render () {
-        const {loadedCity, renderList} = this.state;
-        const {cityScores, city} = this.props;
-
-        if(loadedCity !== city) {
-            this.updateScores(cityScores, city);
-        }
-        
-        return (
-            <div>{renderList}</div>
-        )
-    }
-
+        return <div>{renderList}</div>;
 }
 
 export default LifeQuality;

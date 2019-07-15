49b3f2002a3ffd75ad89736b37cde8c45754d979
abrown945 <abrown945@gmail.com>
2019-07-15 10:49:08 -0400

    responsive search bar

--- src/views/landing.js
+++ src/views/landing.js
@@ -16,49 +16,48 @@ class LandingPage extends React.Component {
 
   handleForm = () => {
     this.props.getCity(this.state.city);
-    this.props.history.push("/citypage");
   };
 
   render() {
     return (
-      <div className="landing-container d-flex align-items-center">
-        <div className="row mx-auto">
-          <div className="landing col-12 col-md-10 mx-auto row align-items-center landing-content pt-5 pb-5">
-            <div className="col-12 col-md-10 mx-auto d-flex justify-content-center p-3">
-              <Link to="/">
-                <img src={logo} alt="logo" className="logo" />
-              </Link>
-            </div>
-            <div className="col-12 col-md-10 mx-auto  ">
-              <div className="mx-auto">
-                <p className=" landing-head text-center mx-auto">
-                  Explore A City
-                </p>
+      <div>
+        {/* title to go here */}
+        <div className="landing-container d-flex align-items-center">
+          <div className="row mx-auto">
+            <div className="landing col-12 col-md-10 mx-auto row align-items-center landing-content pt-5 pb-5">
+              <div className="col-12 col-md-10 mx-auto d-flex justify-content-center p-3">
+                <Link to="/">
+                  <img src={logo} alt="logo" className="logo" />
+                </Link>
               </div>
-              <div className="col-7 mx-auto  ">
-
-                <div className="col-md-8 mx-auto">
-                  <Form onSubmit={this.handleForm}>
-                    <InputGroup className="mb-3">
-                      <FormControl
-                        onChange={this.handleInput}
-                        name="cityName"
-                        placeholder="Type the Name of the City"
-                        aria-label="CityName"
-                        aria-describedby="basic-addon2"
-                      />
-                      <InputGroup.Append>
-                        <Button
-                          className="landingbutton"
-                          variant="outline-secondary"
-                          onClick={this.handleForm}
-                        >
-                          <i class="fas fa-search" />
-                        </Button>
-                      </InputGroup.Append>
-                    </InputGroup>
-                  </Form>
+              <div className="col-12 col-md-10 mx-auto">
+                <div className="mx-auto">
+                  <p className=" landing-head text-center mx-auto">
+                    Explore A City
+                </p>
                 </div>
+                  <div className="col-md-8 mx-auto">
+                    <Form onSubmit={this.handleForm}>
+                      <InputGroup className="mb-3">
+                        <FormControl
+                          onChange={this.handleInput}
+                          name="cityName"
+                          placeholder="Type the Name of the City"
+                          aria-label="CityName"
+                          aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                          <Button
+                            className="landingbutton"
+                            variant="outline-secondary"
+                            onClick={this.handleForm}
+                          >
+                            <i class="fas fa-search" />
+                          </Button>
+                        </InputGroup.Append>
+                      </InputGroup>
+                    </Form>
+                  </div>
               </div>
             </div>
           </div>
